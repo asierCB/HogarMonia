6f4d2d390199452452dab7c86c28e00d13b4ec6e
asierCB <asier.camara.bengoechea@gmail.com>
2025-04-13 11:57:42 +0200

    Añadido Formulario ListaCompra

--- core/static/js/script.js
+++ core/static/js/script.js
@@ -1,13 +1,29 @@
-//console.log('¡JavaScript cargado!');
+//Overlay gastos formulario
+const mostrarBtnG = document.getElementById('btn-agregar-gasto')
+const cerrarBtnG = document.getElementById('cerrarFormularioBtnG')
+const overlayG = document.getElementById('formularioOverlayG')
 
-const mostrarBtn = document.getElementById('btn-agregar-gasto')
-const cerrarBtn = document.getElementById('cerrarFormularioBtn')
-const overlay = document.getElementById('formularioOverlay')
+if (mostrarBtnG && cerrarBtnG && overlayG) {
+    mostrarBtnG.addEventListener('click', () => {
+        overlayG.style.display = 'flex';
+    });
 
-mostrarBtn.addEventListener('click', () => {
-    overlay.style.display = 'flex';
-});
+    cerrarBtnG.addEventListener('click', () => {
+        overlayG.style.display = 'none';
+    });
+}
 
-cerrarBtn.addEventListener('click', () => {
-    overlay.style.display = 'none';
-});
+//Overlay lista de la compra formulario
+const mostrarBtnLC = document.getElementById('btn-agregar-lista')
+const cerrarBtnLC = document.getElementById('cerrarFormularioBtnLC')
+const overlayLC = document.getElementById('formularioOverlayLC')
+
+if (mostrarBtnLC && cerrarBtnLC && overlayLC) {
+    mostrarBtnLC.addEventListener('click', () => {
+        overlayLC.style.display = 'flex';
+    });
+
+    cerrarBtnLC.addEventListener('click', () => {
+        overlayLC.style.display = 'none';
+    });
+}
