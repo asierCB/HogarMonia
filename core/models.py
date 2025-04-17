86c0efc249597a530d17cebe0e1c48126f126822
asierCB <asier.camara.bengoechea@gmail.com>
2025-04-18 00:03:23 +0200

    Sistema de Grupos y Usuarios

--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+
+
+class GrupoHogar(models.Model):
+    id_grupo = models.AutoField(primary_key=True)
+    nombre_grupo = models.CharField(max_length=50)
+    codigo_invitacion = models.CharField(max_length=10, unique=True, default=uuid.uuid4().hex[:10].upper())
+    fecha_creacion = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nombre_grupo
+
+    class Meta:
+        verbose_name = "Grupo del Hogar"
+        verbose_name_plural = "Grupos del Hogar"
+
+
+class UsuarioGrupo(models.Model):
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    grupo = models.ForeignKey(GrupoHogar, on_delete=models.CASCADE)
+    fecha_union = models.DateField(auto_now_add=True)
+    rol = models.CharField(max_length=20, default='Miembro')
+
+    def __str__(self):
+        return f"{self.usuario.username} en {self.grupo.nombre_grupo}"
+
+    class Meta:
+        verbose_name = "Usuario en Grupo"
+        verbose_name_plural = "Usuarios en Grupos"
--- core/views.py
+++ core/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from .forms import RegisterForm
+from .models import GrupoHogar
+
 
 # Create your views here.
 
@@ -47,4 +49,66 @@ def logout_view(request):
     return redirect('login')
 
 def perfil(request):
-    return render(request, 'core/perfil.html')
+    from django.contrib.auth.models import User
+    from .models import UsuarioGrupo
+
+    # Datos para el perfil
+    usuario = request.user
+    grupo_usuario = UsuarioGrupo.objects.filter(usuario=usuario).first()
+    grupo = grupo_usuario.grupo if grupo_usuario else None
+    miembros = grupo.usuariogrupo_set.all() if grupo else None
+    rol = grupo_usuario.rol if grupo_usuario else None
+
+    #Gestión Grupos a través del perfiles
+
+    if request.method == 'POST':
+        accion = request.POST.get('accion')
+
+        if accion == 'crear':
+            nombre_grupo = request.POST.get('nombre_grupo')
+
+            #Se crea el elemento en la tabla GrupoHogar
+            grupo = GrupoHogar.objects.create(nombre_grupo=nombre_grupo)
+
+            #Se crea el elemento en la tabla UsuarioGrupo
+            UsuarioGrupo.objects.create(
+                usuario=request.user,
+                grupo=grupo,
+                rol='Admin'
+            )
+            return redirect('perfil')
+        elif accion == 'unirse':
+            codigo_grupo = request.POST.get('codigo_hogar')
+
+            try:
+                grupo = GrupoHogar.objects.get(codigo_invitacion=codigo_grupo)
+
+                #Verificamos que no este el usuario ya en el grupo
+                ya_en_grupo = UsuarioGrupo.objects.filter(usuario=request.user, grupo=grupo).exists()
+
+                if not ya_en_grupo:
+                    UsuarioGrupo.objects.create(usuario=request.user, grupo=grupo)
+                    print("Unido con exito")
+                    return redirect('perfil')
+                    # Redirigir o recargar con mensaje de éxito
+                else:
+                    pass
+            except GrupoHogar.DoesNotExist:
+                # Mostrar mensaje de error
+                print("Grupo no encontrado")
+                pass
+
+        elif accion == 'salir':
+            grupo_usuario = UsuarioGrupo.objects.filter(usuario=request.user).first()
+
+            if grupo_usuario:
+                grupo = grupo_usuario.grupo
+                grupo_usuario.delete()
+
+                if grupo.usuariogrupo_set.count() == 0:
+                    grupo.delete()
+
+                return redirect('perfil')
+
+
+    return render(request, 'core/perfil.html',{'grupo' : grupo,'miembros' : miembros, 'rol' : rol})
